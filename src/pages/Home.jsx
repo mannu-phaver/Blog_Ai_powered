9ed6220c76385520e8c7bf18f0f9dcefd51bc72f
manu <somamanojshm.22@gmail.com>
2025-06-20 08:17:34 +0530

    Sample blogs were added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
+// import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
